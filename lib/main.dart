dc3d785d38a96a4d13faad97535825a4828620f7
Spreethy <preethysrinevasan@gmail.com>
2025-02-08 20:30:34 -0800

    Add code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
@@ -42,25 +43,124 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: SingleChildScrollView(
-            child: Column(
-      children: <Widget>[
-        Container(
-            height: size.height * 0.40,
-            width: double.infinity,
-            decoration: const BoxDecoration(color: Colors.red),
-            padding:
-                const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              const Text("BMI",
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 60.0,
-                  ))
-            ]))
-      ],
-    )));
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: size.height * 0.40,
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.red),
+              padding:
+                  const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text("BMI",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 60.0,
+                      )),
+                  const Text(
+                    "Calculator",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 40.0,
+                    ),
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      _bmi > 0 ? _bmi.toStringAsFixed(1) : '0.0',
+                      style: const TextStyle(
+                        color: Colors.pink,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 45.0,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                  RichText(
+                    text: TextSpan(
+                      text: "Condition: ",
+                      style:
+                          const TextStyle(color: Colors.pink, fontSize: 25.0),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: _condition,
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              decoration: const BoxDecoration(color: Colors.blue),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+              width: double.infinity,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(height: size.height * 0.05),
+                  const Text(
+                    "Choose Data",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.08),
+                  Text("Height: ${_height.toStringAsFixed(1)} cm"),
+                  Slider(
+                    value: _height,
+                    min: 100.0,
+                    max: 250.0,
+                    divisions: 500,
+                    label: _height.toStringAsFixed(1),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _height = newValue;
+                      });
+                    },
+                  ),
+                  SizedBox(height: size.height * 0.08),
+                  Text("Weight: ${_weight.toStringAsFixed(1)} kg"),
+                  Slider(
+                    value: _weight,
+                    min: 0.0,
+                    max: 200.0,
+                    divisions: 500,
+                    label: _weight.toStringAsFixed(1),
+                    onChanged: (double newValue) {
+                      setState(() {
+                        _weight = newValue;
+                      });
+                    },
+                  ),
+                  SizedBox(height: size.height * 0.08),
+                  TextButton(
+                    onPressed: _calculateBMI,
+                    style: TextButton.styleFrom(
+                        backgroundColor:
+                            const Color.fromARGB(255, 19, 210, 25)),
+                    child: const Text(
+                      'Calculate BMI',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
